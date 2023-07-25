c5175be879c22562256e8b234825d68347f9dc17
NishikiChisato <1484625730@qq.com>
2023-07-25 17:33:58 +0800

    Site updated: 2023-07-25 17:33:42

---
+++ js/tabs.js
@@ -0,0 +1,11 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function() {
+	if (document.hidden) {
+		$('[rel="icon"]').attr('href', "/funny.ico");
+		document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+		clearTimeout(titleTime);
+	} else {
+		$('[rel="icon"]').attr('href', "/img/newtubiao.png");
+		document.title = '(ฅ>ω
